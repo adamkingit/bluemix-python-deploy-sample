19acfc39c75877b94d061e6a08ed033d2256c012
Adam King <rak@us.ibm.com>
2017-02-03 14:33:47 -0500

    pip install pycrypto

--- bin/ansibleSetup.sh
+++ bin/ansibleSetup.sh
@@ -3,5 +3,6 @@
 echo "----------------------------------- python version -----------------------------------"
 python --version
 
-sudo apt-get -y install python python-dev python-setuptools software-properties-common
+sudo apt-get -y install autoconf g++ python2.7-dev python-setuptools software-properties-common
+sudo pip install pycrypto
 sudo pip install ansible
