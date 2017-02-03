51f201e599408dd2e73eb189ade40ce1277ffe8d
Adam King <rak@us.ibm.com>
2017-02-03 14:30:41 -0500

    install python

--- bin/ansibleSetup.sh
+++ bin/ansibleSetup.sh
@@ -3,5 +3,5 @@
 echo "----------------------------------- python version -----------------------------------"
 python --version
 
-sudo apt-get -y install python-dev python-setuptools software-properties-common
+sudo apt-get -y install python python-dev python-setuptools software-properties-common
 sudo pip install ansible
