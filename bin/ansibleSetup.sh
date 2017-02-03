4206c58aa0db3e56694737da7c1617e81b50b8c1
Adam King <rak@us.ibm.com>
2017-02-03 14:35:57 -0500

    fix apt-get

--- bin/ansibleSetup.sh
+++ bin/ansibleSetup.sh
@@ -3,6 +3,6 @@
 echo "----------------------------------- python version -----------------------------------"
 python --version
 
-sudo apt-get -y install autoconf g++ python2.7-dev python-setuptools software-properties-common
+sudo apt-get -y install g++ python-dev python-setuptools software-properties-common
 sudo pip install pycrypto
 sudo pip install ansible
