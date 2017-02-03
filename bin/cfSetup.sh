12833db7429aff16757083a7aa6039eaa414ff7a
Adam King <rak@us.ibm.com>
2017-02-03 14:02:38 -0500

    Use sudo after all

--- bin/cfSetup.sh
+++ bin/cfSetup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "download and install cloudfoundry cli"
-wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | apt-key add -
-echo "deb http://packages.cloudfoundry.org/debian stable main" | tee /etc/apt/sources.list.d/cloudfoundry-cli.list
-apt-get -y update
-apt-get -y install cf-cli
+wget --output-document=cfcli.tgz  https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.23.1&source=github-rel
+tar -xzvf cfcli.tgz
+export PATH=./cf:$PATH
+which cf
