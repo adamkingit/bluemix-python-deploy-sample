816eed2154db893d8d0931c8f30629d26ec2fd83
Adam King <rak@us.ibm.com>
2017-02-03 13:38:11 -0500

    remove sudo

--- bin/cfSetup.sh
+++ bin/cfSetup.sh
@@ -3,5 +3,5 @@
 echo "download and install cloudfoundry cli"
 wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
 echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
-sudo apt-get -y update
-sudo apt-get -y install cf-cli
+apt-get -y update
+apt-get -y install cf-cli
