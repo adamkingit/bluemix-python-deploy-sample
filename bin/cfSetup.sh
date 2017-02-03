f50fc3eb8904b4c9a095151ffe60304e5528c9e1
Adam King <rak@us.ibm.com>
2017-02-03 13:40:43 -0500

    Remove missed suso

--- bin/cfSetup.sh
+++ bin/cfSetup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "download and install cloudfoundry cli"
-wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
-echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | apt-key add -
+echo "deb http://packages.cloudfoundry.org/debian stable main" | tee /etc/apt/sources.list.d/cloudfoundry-cli.list
 apt-get -y update
 apt-get -y install cf-cli
